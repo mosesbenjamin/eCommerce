c7f74653a9fcad9714400acc8302dcb13ffa6599
mosesbenjamin <mavewreek@gmail.com>
2020-10-13 13:47:39 +0100

    Creating a store

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+// import { createStore, applyMiddleware, compose } from 'redux'
+import store from './store'
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-import reducers from '../src/redux/reducers'
+// import reducers from '../src/redux/reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers,
-  composeEnhancers(applyMiddleware()))
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore(reducers,
+//   composeEnhancers(applyMiddleware()))
 
 ReactDOM.render(
   <Provider store={store}>
