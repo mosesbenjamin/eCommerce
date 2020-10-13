b4154ba6bc71e4e36e06471aa2ae0dca1b01d9cc
mosesbenjamin <mavewreek@gmail.com>
2020-10-13 13:29:22 +0100

    Custom error handling

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const { connectDB } = require('./config/db');
 const colors = require('colors');
 
 const {productRoutes} = require('./routes/productRoutes');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 
 dotenv.config()
 
@@ -11,7 +12,11 @@ connectDB()
 
 const app = express();
 
-app.use('/api/products', productRoutes)
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
