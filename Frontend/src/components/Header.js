bfdef14fd39e26021ba9601c6f410a8f3b8ace14
mosesbenjamin <mavewreek@gmail.com>
2020-10-25 16:41:32 +0100

    User register reducer, action and screen

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -13,6 +13,7 @@ const Header = () => {
     const logoutHandler = ()=>{
         dispatch(logout());
     }
+    
     return (
         <header>
             <Navbar bg="dark"  variant="dark" expand="lg" collapseOnSelect>
