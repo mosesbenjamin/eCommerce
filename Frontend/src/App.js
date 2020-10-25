0eb7d83dc59c0635e53e5742a6c22d0564f8ee10
mosesbenjamin <mavewreek@gmail.com>
2020-10-25 15:36:45 +0100

    User login screen and functionality

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Container>
           <Route path='/' exact component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
