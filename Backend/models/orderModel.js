647d88fe6baa7ccb79fa620193934bebf563352c
mosesbenjamin <mavewreek@gmail.com>
2020-10-28 00:31:19 +0100

    Order Screen

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -40,6 +40,11 @@ const orderSchema = mongoose.Schema({
         required: true,
         default: 0.0
     },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
     totalPrice: {
         type: Number,
         required: true,
