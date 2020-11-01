8349501061f0ce8daf932417c782f99269a7c7de
mosesbenjamin <mavewreek@gmail.com>
2020-11-01 16:00:29 +0100

    Admin delete products

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import {productListReducer, productDetailsReducer} from './reducers/productReducers'
+import {
+    productListReducer, 
+    productDetailsReducer,
+    productDeleteReducer
+} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { 
     userLoginReducer, 
@@ -19,6 +23,7 @@ import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyRe
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
