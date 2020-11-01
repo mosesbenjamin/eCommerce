6f7d7661de6922830e09fc50747439a6d23c5743
mosesbenjamin <mavewreek@gmail.com>
2020-11-01 20:12:34 +0100

    Image upload config and endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { connectDB } = require('./config/db');
 const colors = require('colors');
+const path = require('path');
 
+const {uploadRoutes} = require('./routes/uploadRoutes')
 const {productRoutes} = require('./routes/productRoutes');
 const {userRoutes} = require('./routes/userRoutes');
 const {orderRoutes} = require('./routes/orderRoutes');
@@ -20,9 +22,11 @@ app.use(express.json())
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/uploads', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res)=> res.send(process.env.PAYPAL_CLIENT_ID))
 
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound);
 app.use(errorHandler);
