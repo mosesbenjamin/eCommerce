0a7338aef9ddde902103a07ebf9ce55376419500
mosesbenjamin <mavewreek@gmail.com>
2020-11-15 14:43:18 +0100

    Top products carousel

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -7,12 +7,14 @@ const {
     deleteProduct, 
     createProduct, 
     updateProduct ,
-    createProductReview
+    createProductReview,
+    getTopProducts
 } = require('../controllers/productController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 
